ef6c43ae4e7ed4cbb9d2627dde8b630ce750c3ba
Shikhar Srivastava <shikhars2006@gmail.com>
2024-01-27 23:02:38 +0000

    Added getters, setters and attributes to Player class

--- Player.java
+++ Player.java
@@ -1,3 +1,26 @@
 public class Player {
     
+    //attributes
+    private int caveNum;
+    private int qtyArrows;
+
+    //getters and setters
+    public int getCaveNum() {
+        return caveNum;
+    }
+    public void setCaveNum(int caveNum) {
+        this.caveNum = caveNum;
+    }
+    public int getQtyArrows() {
+        return qtyArrows;
+    }
+    public void setQtyArrows(int qtyArrows) {
+        this.qtyArrows = qtyArrows;
+    }
+
+    //constructor
+    public Player(int caveNum, int qtyArrows) {
+        this.caveNum = caveNum;
+        this.qtyArrows = qtyArrows;
+    }
 }
