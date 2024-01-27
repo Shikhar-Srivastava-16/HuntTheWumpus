6c24d21ed22b1f9312da9d8128d888888fdbcaf4
Shikhar Srivastava <shikhars2006@gmail.com>
2024-01-27 11:45:45 +0000

    initial commit

---
+++ CaveSystem.java
@@ -0,0 +1,9 @@
+package P1;
+
+public class CaveSystem {
+    /*
+     * Cave system is a map
+     * each key is a cave 
+     * valus is a list of all caves to which pathways are open.
+     */
+}
---
+++ Wumpus.java
@@ -0,0 +1,27 @@
+public class Wumpus {
+
+    //attributes
+    int cellNo;
+    boolean sleep = true;
+
+    //getters and setters
+    public int getCellNo() {
+        return cellNo;
+    }
+    public void setCellNo(int cellNo) {
+        this.cellNo = cellNo;
+    }
+    public boolean isSleep() {
+        return sleep;
+    }
+    public void setSleep(boolean sleep) {
+        this.sleep = sleep;
+    }
+
+    public void checkMovableSpaces(int currentCellNo) {
+
+        
+
+    }
+    
+}
