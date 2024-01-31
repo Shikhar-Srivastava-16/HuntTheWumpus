ee235b308c6dfb3d086751bf72926e4a17878576
Shikhar Srivastava <shikhars2006@gmail.com>
2024-01-31 20:27:41 +0000

    added top-level Scanner and Random objects to main added code for generation of player added constructor for player

--- main.java
+++ main.java
@@ -4,8 +4,27 @@ public class main {
 
     public static void main(String[] args) {
         
+        //Scanner for reading user input
+        Scanner inputReader = new Scanner(System.in);
+        //Randomiser
+        Random randomiser = new Random();
+
         //Generating map:
         CaveSystem caveSystem1 = new CaveSystem(1);
-        
+
+        //Generate player
+        //int numArrows = ;
+        int caveNum = 0;
+        while (caveNum == 0) {
+            int num = 1 + randomiser.nextInt(20);
+            if (!(caveSystem1.getPitLocation().contains(num) 
+            || caveSystem1.getArrowLocation().contains(num)
+            || caveSystem1.getWumpLocation().contains(num))) {
+                caveNum = num;
+            }
+        }
+
+        Player player = new Player(caveNum, 3);
+
     }
 }
