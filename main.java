f5507a40719408bbc3299d8a22468b366b40ee1c
Shikhar Srivastava <shikhars2006@gmail.com>
2024-02-01 01:48:47 +0000

    1. Moved Scanner and Reader objects to be static attributes declared outside main method 2. Changed player generation to use newly added CaveSystem.generatePlayerCave() method 3. Added methods to perform checks in traversal and finished code for traversal

--- main.java
+++ main.java
@@ -1,30 +1,66 @@
 import java.util.*;
 
 public class main {
+    
+    //Scanner for reading user input
+    private static Scanner inputReader = new Scanner(System.in);
+    //Randomiser
+    private static Random randomiser = new Random();
 
     public static void main(String[] args) {
-        
-        //Scanner for reading user input
-        Scanner inputReader = new Scanner(System.in);
-        //Randomiser
-        Random randomiser = new Random();
 
         //Generating map:
         CaveSystem caveSystem1 = new CaveSystem(1);
 
         //Generate player
-        //int numArrows = ;
-        int caveNum = 0;
-        while (caveNum == 0) {
-            int num = 1 + randomiser.nextInt(20);
-            if (!(caveSystem1.getPitLocation().contains(num) 
-            || caveSystem1.getArrowLocation().contains(num)
-            || caveSystem1.getWumpLocation().contains(num))) {
-                caveNum = num;
-            }
-        }
-
+        int caveNum = caveSystem1.generatePlayerCave(randomiser);
         Player player = new Player(caveNum, 3);
 
+        //Traversal
+        while (true) {
+            player.movePlayer(caveSystem1, inputReader);
+            int playerLocation = player.getCaveNum();
+
+            checkForEvent(caveSystem1, playerLocation, player);
+            checkForSenses(playerLocation, caveSystem1);
+        }
+    }
+
+    public static void checkForEvent(CaveSystem caveSystem1, int playerLocation, Player player) {
+
+        if (caveSystem1.getPitLocation().contains(playerLocation)) {
+            System.out.println("Falls.");
+            System.exit(0);
+        }
+
+        //check for Bat
+        if (caveSystem1.getBatLocation().contains(playerLocation)) {
+            System.out.println("Bat.");
+            //bat can drop you onto any empty square.
+            player.setCaveNum(caveSystem1.generatePlayerCave(randomiser));
+            System.out.printf("new location: %d", player.getCaveNum());
+            System.out.println("Now in cave " + player.getCaveNum());
+        }
+
+        //check for arrow
+        if (caveSystem1.getArrowLocation().contains(playerLocation)) {
+            System.out.println("pokey stick.");
+            int currentArrows = player.getQtyArrows();
+            player.setQtyArrows(currentArrows++);
+        }
+    }
+
+    public static void checkForSenses(int caveNum, CaveSystem caveSystem) {
+
+        HashSet<Integer> nearbyCaves = caveSystem.getNearbyCaves(caveNum);
+        for (int cave : nearbyCaves) {
+            if (caveSystem.getWumpLocation().contains(cave)) {
+                System.out.println("smell");
+            } else if (caveSystem.getBatLocation().contains(cave)){
+                System.out.println("flap");
+            } else if (caveSystem.getPitLocation().contains(cave)) {
+                System.out.println("breeze");
+            }
+        } 
     }
 }
